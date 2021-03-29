4df778a52b313315a8060c67a2caced394187ef3
vtorun <volkantorunn@hotmail.com>
2021-03-29 09:12:57 +0300

    DictionaryDemos
    DictionaryDemos

---
+++ MyListDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyListDictionary<Key, Value>
+    {
+        (Key, Value)[] items;
+
+        public MyListDictionary()
+        {
+            items = new (Key, Value)[0];
+        }
+
+        public void Add(Key _key, Value _value)
+        {
+            (Key, Value)[] tempArray = items;
+            items = new (Key, Value)[tempArray.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = (_key, _value);
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public void Listing() 
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+        
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyListDictionary<int, string> futbolcular = new MyListDictionary<int, string>();
+            futbolcular.Add(27,"Dimitar Ivankov");
+            futbolcular.Add(04, "Ömer Erdoğan");
+            futbolcular.Add(21, "Ali Tandoğan");
+            futbolcular.Add(17, "Pablo Batalla");
+            futbolcular.Add(10, "Volkan Şen");
+            futbolcular.Add(09, "Sercan Yıldırım");
+
+            Console.WriteLine(futbolcular.Length);
+            futbolcular.Listing();
+
+        }
+    }
+}
